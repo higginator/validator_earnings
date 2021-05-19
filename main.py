f92d37b3b1f42c4241aca8de62526b5fa68fbad3
Ryan Higgins <ryanhigs@gmail.com>
2021-05-19 15:06:27 -0700

    flask web app

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, json
+import requests
+from thousand_validators import *
+
+app = Flask(__name__)
+
+"""
+    Endpoint returns a JSON string of the validators in the Polkadot Thousand Validators Programme.
+"""
+@app.route('/validators/polkadot')
+def polkadot_validators():
+    validators = get_polkadot_validators()
+    return json.dumps(validators)
+
+"""
+    Endpoint returns a JSON string of the validators in the Kusama Thousand Validators Programme.
+"""
+@app.route('/validators/kusama')
+def kusama_validators():
+    validators = get_kusama_validators()
+    return json.dumps(validators)
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
